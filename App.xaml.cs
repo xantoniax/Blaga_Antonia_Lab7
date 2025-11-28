2b8191eff70029bb741b4025426124e92c3cc8c3
Antonia Blaga <antonia_blaga07@yahoo.com>
2025-11-29 00:54:07 +0200

    Laborator8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,11 +1,33 @@
-﻿namespace BlagaAntoniaLab7
+﻿using System;
+using BlagaAntoniaLab7.Data;
+using System.IO;
+
+namespace BlagaAntoniaLab7
 {
     public partial class App : Application
     {
+
+        static ShoppingListDatabase database;
+ public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
+            MainPage = new AppShell();
         }
+
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
---
+++ ListPage.xaml.cs
@@ -0,0 +1,28 @@
+using BlagaAntoniaLab7.Models;
+using System;
+namespace BlagaAntoniaLab7
+{
+
+    public partial class ListPage : ContentPage
+    {
+        public ListPage()
+        {
+            InitializeComponent();
+        }
+        async void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            var slist = (ShopList)BindingContext;
+            slist.Date = DateTime.UtcNow;
+            await App.Database.SaveShopListAsync(slist);
+            await Navigation.PopAsync();
+        }
+        async void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            var slist = (ShopList)BindingContext;
+            await App.Database.DeleteShopListAsync(slist);
+            await Navigation.PopAsync();
+        }
+
+    }
+
+}
